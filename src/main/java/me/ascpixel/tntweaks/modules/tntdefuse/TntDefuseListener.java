b3cb5b5f1ffa742f83f48de60dcb507aaa11fa85
ascpixel <kbartek05@gmail.com>
2021-03-18 00:34:14 +0100

    Damage defusal item on defuse
    Makes the defusal item be damaged 1 durability point if they can be damaged

--- src/main/java/me/ascpixel/tntweaks/modules/tntdefuse/TntDefuseListener.java
+++ src/main/java/me/ascpixel/tntweaks/modules/tntdefuse/TntDefuseListener.java
@@ -10,6 +10,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 /**
  * The listener for the water defusing feature.
@@ -27,15 +29,27 @@ final class TntDefuseListener implements Listener {
             return;
 
         Entity entity = event.getRightClicked();
-        Player p = event.getPlayer();
 
         // The event triggers 2 times for HAND and OFFHAND
         if(!event.getHand().equals(EquipmentSlot.HAND)) return;
 
+        Player p = event.getPlayer();
+        ItemStack item = p.getInventory().getItemInMainHand();
+
         if(entity.getType() == EntityType.PRIMED_TNT &&
-            p.getInventory().getItemInMainHand().getType() == module.getDefuseMaterial()
+                item.getType() == module.getDefuseMaterial()
         ){
             World world = entity.getWorld();
+
+            // Decrease durability on damageable items
+            ItemMeta meta = item.getItemMeta();
+            if(meta instanceof Damageable){
+                Damageable damageable = (Damageable)meta;
+                damageable.setDamage(damageable.getDamage() + 1);
+
+                item.setItemMeta(meta);
+                p.getInventory().setItemInMainHand(item);
+            }
 
             // Remove the TNT
             entity.remove();
